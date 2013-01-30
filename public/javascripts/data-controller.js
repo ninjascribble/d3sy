095bdf3d69b269b6ad104dde59d69cbb2a931c12
Scott Grogan <scott@ninjascript.com>
2013-01-30 13:33:27 -0800

    Modifying the data model to keep track of values over time

--- public/javascripts/data-controller.js
+++ public/javascripts/data-controller.js
@@ -1,8 +1,64 @@
 ;(function() {
 	"use strict";
 
-	var _console = $('pre');
+	var _root = $('pre')
+	  , _data = XYZ.model;
 
-	_console.text('Data Controller says "hi"')
+	for (var i = 0; i < 24 * 365; i++) {
+		XYZ.step();
+	}
+
+	_root.text(JSON.stringify(XYZ.model, 0, 2));
+	setInterval(function() {
+		XYZ.step();
+		_root.text(JSON.stringify(XYZ.model, 0, 2));
+	}, 100);
+
+	// ----------------------------------
+
+	var _data = {
+
+		sales: {
+
+			hourly: [
+				{ date: new Date('1/1/2012 0:00'), value: 101 },
+				{ date: new Date('1/1/2012 1:00'), value: 101 },
+				{ date: new Date('1/1/2012 2:00'), value: 101 },
+				{ date: new Date('1/1/2012 3:00'), value: 101 },
+				{ date: new Date('1/1/2012 4:00'), value: 101 },
+				{ date: new Date('1/1/2012 5:00'), value: 101 },
+				{ date: new Date('1/1/2012 6:00'), value: 101 },
+				{ date: new Date('1/1/2012 7:00'), value: 101 },
+				{ date: new Date('1/1/2012 8:00'), value: 101 },
+				{ date: new Date('1/1/2012 9:00'), value: 101 },
+				{ date: new Date('1/1/2012 10:00'), value: 101 },
+				{ date: new Date('1/1/2012 11:00'), value: 101 },
+				{ date: new Date('1/1/2012 12:00'), value: 101 },
+				{ date: new Date('1/1/2012 13:00'), value: 101 },
+				{ date: new Date('1/1/2012 14:00'), value: 101 },
+				{ date: new Date('1/1/2012 15:00'), value: 101 },
+				{ date: new Date('1/1/2012 16:00'), value: 101 },
+				{ date: new Date('1/1/2012 17:00'), value: 101 },
+				{ date: new Date('1/1/2012 18:00'), value: 101 },
+				{ date: new Date('1/1/2012 19:00'), value: 101 },
+				{ date: new Date('1/1/2012 20:00'), value: 101 },
+				{ date: new Date('1/1/2012 21:00'), value: 101 },
+				{ date: new Date('1/1/2012 22:00'), value: 101 },
+				{ date: new Date('1/1/2012 23:00'), value: 101 },
+			],
+
+			daily: [
+				{ date: new Date('1/1/2012 0:00'), value: 2424 }
+			],
+
+			weekly: [
+				{ date: new Date('1/1/2012 0:00'), value: 2424 }
+			],
+
+			monthly: [
+				{ date: new Date('1/1/2012 0:00'), value: 2424 }
+			]
+		}
+	}
 
 }());
--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -1,12 +1,11 @@
 var XYZ = XYZ || {};
 
 (function() {
-  var regions = ['west'],
-      storesPerRegion = [1, 1],
-      scoreRange = [1, 5],
-      products = ['XBox 360', 'Microsoft Surface', 'Contoso Knives', "Contoso Women's Jeans", '2% Milk', 'Cookies', 'Contoso Microwave Oven', 'Contoso Chicken Strips', 'Cheese', 'Contoso Gloves', 'Contoso Hair Dryer'],
+
+  var products = ['XBox 360', 'Microsoft Surface', 'Contoso Knives', "Contoso Women's Jeans", '2% Milk', 'Cookies', 'Contoso Microwave Oven', 'Contoso Chicken Strips', 'Cheese', 'Contoso Gloves', 'Contoso Hair Dryer'],
       categories = ['food', 'appliances', 'apparel', 'electronics'],
       categoryProducts = {'food': '2% Milk', 'appliances': 'Contoso Knives', 'electronics': 'Microsoft Surface', 'apparel': "Contoso Women's Jeans"},
+      slices = ['foot traffic', 'sales', 'returns']
       baseFacebookRate = 100,
       baseFootTrafficRate = 500,
       baseTwitterRate = 50,
@@ -16,188 +15,73 @@ var XYZ = XYZ || {};
       basePromotionRate = 5000,
       baseSalesRate = 20000,
       baseProductSalesRate = 100,
-      baseReturnsRate = 2,
-      promotionRange = [4, 8],
-      lowInventoryRange = [3, 11],
-      storeHours = [0, 23],
-      returnCount = 8,
-      data = [],
-      cities = ['Bellevue', 'Redmond', 'Kirkland', 'Seattle', 'Portland', 'New York', 'New Orleans', 'Chicago', 'San Francisco', 'Los Angeles']
-      comments = [
-        {
-          'content': 'volutpat enim accumsan sit lobortis',
-          'context': 'Facebook',
-          'impact': {
-            'comments': Math.round(Math.random() * 100),
-            'likes': Math.round(Math.random() * 100)
-          }
-        },
-        {
-          'content': 'volutpat enim accumsan sit lobortis',
-          'context': 'Facebook',
-          'impact': {
-            'comments': Math.round(Math.random() * 100),
-            'likes': Math.round(Math.random() * 100)
-          }
-        },
-        {
-          'content': 'volutpat enim accumsan sit lobortis',
-          'context': 'Facebook',
-          'impact': {
-            'comments': Math.round(Math.random() * 100),
-            'likes': Math.round(Math.random() * 100)
-          }
-        },
-        {
-          'content': 'volutpat enim accumsan sit lobortis',
-          'context': 'Twitter',
-          'impact': {
-            'retweets': Math.round(Math.random() * 100),
-            'replies': Math.round(Math.random() * 100)
-          }
-        },
-        {
-          'content': 'volutpat enim accumsan sit lobortis',
-          'context': 'Twitter',
-          'impact': {
-            'retweets': Math.round(Math.random() * 100),
-            'replies': Math.round(Math.random() * 100)
-          }
-        }
-      ]
+      baseReturnsRate = 2;
 
-  function generateData(hourlyBase) {
-    var current = hourlyBase * (1 + ((Math.random() - 0.5) / 5)),
-        date = new Date(),
-        day = generateTimescale(storeHours[0], storeHours[1], current),
-        week = generateTimescale(date.getDay() - 7, date.getDay() - 1, sumTimescale(day)),
-        month = generateTimescale(Math.floor(date.getDate() / 7) - 4, Math.floor(date.getDate() / 7) - 1, sumTimescale(week)),
-        year = generateTimescale(date.getMonth() - 12, date.getMonth() - 1, sumTimescale(month));
-    return {'current': current, 'past': {'day': day, 'week': week, 'month': month, 'year': year}};
-  }
+    var currentDate = new Date('12/31/2011 23:00').getTime()
+      , model = { hourly: [], daily: [], weekly: [], monthly: [] };
 
-    // Fill data from start to end with 20% variance at each step.
-    function generateTimescale(start, end, baseValue) {
-      var data = [{'time': start, 'value': baseValue * (1 + ((Math.random() - 0.5) / 5))}],
-          range = end - start;
-      for (var i = 1; i <= range; i++) {
-        data.push({'time': i + start, 'value': data[i - 1].value * (1 + ((Math.random() - 0.5) / 5))});
-      }
-      return data;
-    }
+    function step() {
 
-  function sumTimescale(data) {
-    var total = 0;
-    for (var i = 0; i < data.length; i++) total += data[i].value;
-    return total;
-  }
+        currentDate += (1000 * 60 * 60);
 
-  function shuffle(a) {
-      for(var j, x, i = a.length; i; j = parseInt(Math.random() * i), x = a[--i], a[i] = a[j], a[j] = x);
-      return a;
-  }
-  
-  function makeData() {
+        var curhour    = model.hourly[ model.hourly.length - 1 ]
+          , curday     = model.daily[ model.daily.length - 1 ]
+          , curweek    = model.weekly[ model.weekly.length - 1 ]
+          , curmonth   = model.monthly[ model.monthly.length - 1 ]
+          , date       = new Date(currentDate)
+          , value      = Math.random() * 150 + 50 << 0;
 
-    data = [];
-
-    for (var r = 0; r < regions.length; r++) {
-      var region = regions[r],
-          storeCount = Math.round(Math.random() * (storesPerRegion[1] - storesPerRegion[0]) + storesPerRegion[0]),
-          regionCities = shuffle(cities.slice(0));
-      for (var s = 0; s < storeCount; s++) {
-        var city = regionCities.pop();
-        var storeInfo = {
-          'name': city,
-          'photo': 'images/store_' + s + '.png',
-          'address': {'street': '42 Street Dr.', 'city': city, 'state': 'WA'},
-          'phone': '206-555-1000',
-          'contact': {'name': 'Boss Manager', 'phone': '206-555-4242', 'email': 'manager@contoso.com'},
-          'description': 'Esse ipsum aliquip facilisi eum ipsum, volutpat enim accumsan sit lobortis, volutpat esse vel augue. Volutpat, commodo ea aliquip elit praesent, wisi zzril eu eros ut.'
+        // Update hours
+        model.hourly.push(createEntry(date, value));
+        if (model.hourly.length > 24) {
+            model.hourly.shift();
         }
 
-        data.push({
-          'id': region + '_' + s,
-          'region': region,
-          'storeInfo': storeInfo,
-          'sentiment': Math.random() * (scoreRange[1] - scoreRange[0]) + scoreRange[1],
-          'socialReach': {
-            'facebook': generateData(baseFacebookRate),
-            'twitter': generateData(baseTwitterRate),
-            'foursquare': generateData(baseFoursquareRate),
-            'featured': shuffle(comments).slice(Math.round(Math.random() * 2))
-          },
-          'footTraffic': generateData(baseFootTrafficRate),
-          'revenue': {
-            'actual': generateData(baseRevenueRate),
-            'forecasted': generateData(baseRevenueRate),
-          },
-          'topProducts': (function() {
-            var topProducts = {};
-            for (var i = 0; i < categories.length; i++) {
-              topProducts[categories[i]] = {'product': categoryProducts[categories[i]], 'data': generateData(baseProductSalesRate)}
-            }
-            return topProducts;
-          })(),
-          'inStorePickup': (function() {
-            var pickups = {};
-            for (var i = 0; i < categories.length; i++) {
-              pickups[categories[i]] = generateData(baseInStorePickupRate);
-            }
-            return pickups;
-          })(),
-          'promotions': {
-            'promotions': generateData(basePromotionRate),
-            'sales': generateData(baseSalesRate),
-            'products': (function(){
-              var promotionProducts = shuffle(products).slice(0, Math.round(Math.random() * (promotionRange[1] - promotionRange[0])) + promotionRange[0]),
-                  promotions = [];
-              for (var i = 0; i < promotionProducts.length; i++) {
-                var available = Math.round(Math.random() * 1000),
-                    redeemed = Math.round(Math.random() * available);
-                promotions.push({
-                  'promotion': promotionProducts[i] + ' SALE',
-                  'available': available,
-                  'redeemed': redeemed,
-                  'budget': available * 100,
-                  'sales': available * 100
-                });
-              }
-              return promotions;
-            })()
-          },
-          'lowInventory': (function(){
-            var lowProducts = shuffle(products).slice(0, Math.round(Math.random() * (lowInventoryRange[1] - lowInventoryRange[0])) + lowInventoryRange[0]),
-                inventory = [];
-            for (var i = 0; i < lowProducts.length; i++) {
-              var remaining = Math.round() * 50;
-              inventory.push({
-                'product': lowProducts[i],
-                'qty': remaining,
-                'timeLeftEstimate': remaining * 20 * 60 //Time in seconds
-              });
-            }
-            return inventory;
-          })(),
-          'returns': (function(){
-            var returnedProducts = shuffle(products).slice(0, 8),
-                returns = [];
-            for (var i = 0; i < returnedProducts.length; i++) {
-              returns.push({
-                'product': returnedProducts[i],
-                'returns': generateData(baseReturnsRate)
-              })
-            }
-            return returns;
-          })()
-        });
-      }
-    }
-    return data;
-  }
+        // Update days
+        if (curday && date.getDate() == curday.date.getDate()) {
+            curday = addValue(curday, value);
+        }
+        else {
+            model.daily.push(createEntry(date, value));
+        }
+        if (model.daily.length > 7) {
+            model.daily.shift();
+        }
 
-  XYZ.data = makeData();
-  XYZ.makeData = makeData;
-  XYZ.generateData = generateData;
-  XYZ.generateTimescale = generateTimescale;
+        // Update weeks
+        if (curweek && curweek.ct < 7 * 24) {
+            curweek = addValue(curweek, value);
+        }
+        else {
+            model.weekly.push(createEntry(date, value));
+        }
+        if (model.weekly.length > 12) {
+            model.weekly.shift();
+        }
+
+        // Update months
+        if (curmonth && date.getMonth() == curday.date.getMonth()) {
+            curmonth = addValue(curmonth, value);
+        }
+        else {
+            model.monthly.push(createEntry(date, value));
+        }
+        if (model.monthly.length > 12) {
+            model.monthly.shift();
+        }
+    }
+
+    function createEntry(date, value) {
+        return { date: date, value: value, ct: 0 };
+    }
+
+    function addValue(entry, value) {
+        entry.value += value;
+        entry.ct++;
+        return entry;
+    }
+
+  XYZ.model = model;
+  XYZ.step = step;
+
 })();
