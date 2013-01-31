d9426838ce5193b5308dba37b7e0be5492638e87
Scott Grogan <scott@ninjascript.com>
2013-01-30 16:02:33 -0800

    Adding sections to data model

--- public/javascripts/data-controller.js
+++ public/javascripts/data-controller.js
@@ -2,63 +2,21 @@
 	"use strict";
 
 	var _root = $('pre')
-	  , _data = XYZ.model;
+	  , _data = XYZ.model
+	  , _btn = $('button').bind('click', function() {
+	  	XYZ.step();
+	  	_root.text(JSON.stringify(XYZ.model, 0, 2));
+	  });
 
 	for (var i = 0; i < 24 * 365; i++) {
 		XYZ.step();
 	}
+	
+	setInterval(localStep, 300);
 
-	_root.text(JSON.stringify(XYZ.model, 0, 2));
-	setInterval(function() {
+	function localStep() {
 		XYZ.step();
 		_root.text(JSON.stringify(XYZ.model, 0, 2));
-	}, 100);
-
-	// ----------------------------------
-
-	var _data = {
-
-		sales: {
-
-			hourly: [
-				{ date: new Date('1/1/2012 0:00'), value: 101 },
-				{ date: new Date('1/1/2012 1:00'), value: 101 },
-				{ date: new Date('1/1/2012 2:00'), value: 101 },
-				{ date: new Date('1/1/2012 3:00'), value: 101 },
-				{ date: new Date('1/1/2012 4:00'), value: 101 },
-				{ date: new Date('1/1/2012 5:00'), value: 101 },
-				{ date: new Date('1/1/2012 6:00'), value: 101 },
-				{ date: new Date('1/1/2012 7:00'), value: 101 },
-				{ date: new Date('1/1/2012 8:00'), value: 101 },
-				{ date: new Date('1/1/2012 9:00'), value: 101 },
-				{ date: new Date('1/1/2012 10:00'), value: 101 },
-				{ date: new Date('1/1/2012 11:00'), value: 101 },
-				{ date: new Date('1/1/2012 12:00'), value: 101 },
-				{ date: new Date('1/1/2012 13:00'), value: 101 },
-				{ date: new Date('1/1/2012 14:00'), value: 101 },
-				{ date: new Date('1/1/2012 15:00'), value: 101 },
-				{ date: new Date('1/1/2012 16:00'), value: 101 },
-				{ date: new Date('1/1/2012 17:00'), value: 101 },
-				{ date: new Date('1/1/2012 18:00'), value: 101 },
-				{ date: new Date('1/1/2012 19:00'), value: 101 },
-				{ date: new Date('1/1/2012 20:00'), value: 101 },
-				{ date: new Date('1/1/2012 21:00'), value: 101 },
-				{ date: new Date('1/1/2012 22:00'), value: 101 },
-				{ date: new Date('1/1/2012 23:00'), value: 101 },
-			],
-
-			daily: [
-				{ date: new Date('1/1/2012 0:00'), value: 2424 }
-			],
-
-			weekly: [
-				{ date: new Date('1/1/2012 0:00'), value: 2424 }
-			],
-
-			monthly: [
-				{ date: new Date('1/1/2012 0:00'), value: 2424 }
-			]
-		}
 	}
 
 }());
--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -2,86 +2,193 @@ var XYZ = XYZ || {};
 
 (function() {
 
-  var products = ['XBox 360', 'Microsoft Surface', 'Contoso Knives', "Contoso Women's Jeans", '2% Milk', 'Cookies', 'Contoso Microwave Oven', 'Contoso Chicken Strips', 'Cheese', 'Contoso Gloves', 'Contoso Hair Dryer'],
-      categories = ['food', 'appliances', 'apparel', 'electronics'],
-      categoryProducts = {'food': '2% Milk', 'appliances': 'Contoso Knives', 'electronics': 'Microsoft Surface', 'apparel': "Contoso Women's Jeans"},
-      slices = ['foot traffic', 'sales', 'returns']
-      baseFacebookRate = 100,
-      baseFootTrafficRate = 500,
-      baseTwitterRate = 50,
-      baseFoursquareRate = 20,
-      baseRevenueRate = 20000,
-      baseInStorePickupRate = 5,
-      basePromotionRate = 5000,
-      baseSalesRate = 20000,
-      baseProductSalesRate = 100,
-      baseReturnsRate = 2;
+    var ONE_HOUR         = 60 * 60 * 1000
+      , HOURS_PER_DAY    = 24
+      , HOURS_PER_WEEK   = 24 * 7
+      , DAYS_PER_WEEK    = 7
+      , WEEKS_PER_PERIOD = 12
+      , MONTHS_PER_YEAR   = 12;
 
     var currentDate = new Date('12/31/2011 23:00').getTime()
-      , model = { hourly: [], daily: [], weekly: [], monthly: [] };
+      , state = {
+
+        'social-reach': {
+            data: { hourly: [], daily: [], weekly: [], monthly: [] },
+            templates: [
+                { label: 'facebook',   value: 100 },
+                { label: 'foursquare', value:  20 },
+                { label: 'twitter',    value:  50 }
+            ]
+        },
+
+        'foot-traffic': {
+            data: { hourly: [], daily: [], weekly: [], monthly: [] },
+            templates: [
+                { value: 500 }
+            ]
+        },
+
+        'revenue': {
+            data: { hourly: [], daily: [], weekly: [], monthly: [] },
+            templates: [
+                { label: 'net',   value: 100000 },
+                { label: 'gross', value:  60000 }
+            ]
+        },
+
+        'top-products': {
+            data: { hourly: [], daily: [], weekly: [], monthly: [] },
+            templates: [
+                { label: 'appliances',  value: 25000 },
+                { label: 'clothing',    value: 10000 },
+                { label: 'electronics', value: 50000 },
+                { label: 'food',        value: 15000 }
+            ]
+        },
+
+        'pick-ups': {
+            data: { hourly: [], daily: [], weekly: [], monthly: [] },
+            templates: [
+                { value: 30 }
+            ]
+        },
+
+        'promotions': {
+            data: { hourly: [], daily: [], weekly: [], monthly: [] },
+            templates: [
+                { label: 'sales',       value: 40000 },
+                { label: 'promotions',  value: 10000 }
+            ]
+        },
+
+        'returns': {
+            data: { hourly: [], daily: [], weekly: [], monthly: [] },
+            templates: [
+                { label: 'XBox 360',               value: 20 },
+                { label: 'Microsoft Surface',      value: 20 },
+                { label: 'Contoso Knives',         value: 30 },
+                { label: 'Contoso Women\'s Jeans', value: 80 },
+                { label: '2% Milk',                value: 40 },
+                { label: 'Cookies',                value: 30 },
+                { label: 'Contoso Microwave Oven', value: 50 },
+                { label: 'Contoso Chicken Strips', value: 60 },
+                { label: 'Cheese',                 value: 90 },
+                { label: 'Contoso Gloves',         value: 40 },
+                { label: 'Contoso Hair Dryer',     value: 30 }
+            ]
+        }
+
+    };
 
     function step() {
+        currentDate += ONE_HOUR;
+        for (var key in state) {
+            updateState(state[key]);
+        }
+    }
 
-        currentDate += (1000 * 60 * 60);
+    function random(baseValue) {
+        return baseValue * (1 + (Math.random() - .5) / 5) << 0;
+    }
 
-        var curhour    = model.hourly[ model.hourly.length - 1 ]
+    function clone(obj) {
+        var result = JSON.parse( JSON.stringify(obj) );
+        if (result.date) {
+            result.date = new Date(result.date);
+        }
+        return result;
+    }
+
+    function createEntry(date, templates) {
+
+        var result = { date: date, values: [], ct: 0 }
+          , i = 0
+          , len = templates.length
+          , temp;
+
+        for (i; i < len; i++) {
+            temp = templates[i];
+            temp.value = random(temp.value);
+            result.values.push(temp)
+        }
+
+        return result;
+    }
+
+    function addValue(entry, values) {
+
+        var result = entry
+          , i = 0
+          , len = values.length
+          , temp;
+
+        for (i; i < len; i++) {
+            result.values[i].value += values[i].value;
+        }
+
+        entry.ct++;
+
+        return result;
+    }
+
+    function updateState(state) {
+
+        var model      = state.data
           , curday     = model.daily[ model.daily.length - 1 ]
           , curweek    = model.weekly[ model.weekly.length - 1 ]
           , curmonth   = model.monthly[ model.monthly.length - 1 ]
           , date       = new Date(currentDate)
-          , value      = Math.random() * 150 + 50 << 0;
+          , templates  = clone(state.templates)
+          , values;
 
         // Update hours
-        model.hourly.push(createEntry(date, value));
-        if (model.hourly.length > 24) {
+        model.hourly.push(createEntry(date, templates));
+        if (model.hourly.length > HOURS_PER_DAY) {
             model.hourly.shift();
         }
 
+        curhour = model.hourly[ model.hourly.length - 1 ];
+        newvalues = curhour.values;
+
         // Update days
         if (curday && date.getDate() == curday.date.getDate()) {
-            curday = addValue(curday, value);
+            curday = addValue(curday, newvalues);
         }
         else {
-            model.daily.push(createEntry(date, value));
+            model.daily.push( clone(curhour) );
         }
-        if (model.daily.length > 7) {
+        if (model.daily.length > DAYS_PER_WEEK) {
             model.daily.shift();
         }
 
         // Update weeks
-        if (curweek && curweek.ct < 7 * 24) {
-            curweek = addValue(curweek, value);
+        if (curweek && curweek.ct < HOURS_PER_WEEK) {
+            curweek = addValue(curweek, newvalues);
         }
         else {
-            model.weekly.push(createEntry(date, value));
+            model.weekly.push( clone(curhour) );
         }
-        if (model.weekly.length > 12) {
+        if (model.weekly.length > WEEKS_PER_PERIOD) {
             model.weekly.shift();
         }
 
         // Update months
         if (curmonth && date.getMonth() == curday.date.getMonth()) {
-            curmonth = addValue(curmonth, value);
+            curmonth = addValue(curmonth, newvalues);
         }
         else {
-            model.monthly.push(createEntry(date, value));
+            model.monthly.push( clone(curhour) );
         }
-        if (model.monthly.length > 12) {
+        if (model.monthly.length > MONTHS_PER_YEAR) {
             model.monthly.shift();
         }
     }
 
-    function createEntry(date, value) {
-        return { date: date, value: value, ct: 0 };
-    }
-
-    function addValue(entry, value) {
-        entry.value += value;
-        entry.ct++;
-        return entry;
-    }
-
-  XYZ.model = model;
   XYZ.step = step;
+  XYZ.model = {};
+
+  for (var key in state) {
+    XYZ.model[key] = state[key].data;
+  }
 
 })();
